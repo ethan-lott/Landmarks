f5a006a820ff7d9993e90b7338e30f9a9256566d
Ethan Lott <ethan.j.lott@gmail.com>
2023-10-26 17:09:39 -0400

    Done through section 7.

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
     var body: some View {
         Map(coordinateRegion: region)
     }
 
-
    private var region: Binding<MKCoordinateRegion> {
        Binding {
            MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            center: coordinate,
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
            )
        } set: {region in
@@ -28,6 +29,6 @@ struct MapView: View {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
